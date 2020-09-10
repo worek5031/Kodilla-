2f2f5480dbb4bf9d588795028362ac5931571706
Piotr Worsztynowicz <piotr.worsztynowicz@iron-ic.com.pl>
2020-09-10 14:23:30 +0200

    6.2 calculator po poprawkach

--- kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
@@ -3,6 +3,7 @@ package com.kodilla.testing;
 import com.kodilla.testing.user.SimpleUser;
 import com.kodilla.testing.calculator.Calculator;
 
+
 public class TestingMain {
     public static void main(String[] args) {
         SimpleUser simpleUser = new SimpleUser("theForumUser");
@@ -16,9 +17,9 @@ public class TestingMain {
         }
         System.out.println("Test - pierwszy test jednostkowy:");
 
-        Calculator testCalculator = new Calculator(140, 60);
+        Calculator testCalculator = new Calculator();
 
-        if((testCalculator.addAtoB()==210) && (testCalculator.substractBfromA()==80)) {
+        if((testCalculator.addAtoB(140, 60)==204) && (testCalculator.substractBfromA(140, 60)==80)) {
             System.out.println("Test ok");
         }
         else {
--- kodilla-testing/src/main/java/com/kodilla/testing/calculator/Calculator.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/calculator/Calculator.java
@@ -1,19 +1,12 @@
 package com.kodilla.testing.calculator;
 
 public class Calculator {
-   private int A;
-   private int B;
 
-   public Calculator(int A, int B) {
-       this.A = A;
-       this.B = B;
+public int addAtoB(int A, int B) {
+       return A + B;
    }
 
-   public int addAtoB() {
-       return this.A + this.B;
-   }
-
-   public int substractBfromA() {
-       return this.A - this.B;
+   public int substractBfromA(int A, int B) {
+       return A - B;
    }
 }
