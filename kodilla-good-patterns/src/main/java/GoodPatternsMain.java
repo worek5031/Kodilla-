cd0b90aa9844f63e5d2ffd3322e3bd48c334ef6e
Piotr Worsztynowicz <piotr.worsztynowicz@iron-ic.com.pl>
2020-11-26 10:18:40 +0100

    2020-11-26.1

--- kodilla-good-patterns/src/main/java/GoodPatternsMain.java
+++ kodilla-good-patterns/src/main/java/GoodPatternsMain.java
@@ -1,6 +1,14 @@
+import com.kodilla.good.patterns.MovieStore;
 
+import java.util.stream.Collectors;
 
 public class GoodPatternsMain {
+
     public static void main(String[] args) {
+        MovieStore movieStore = new MovieStore();
+        String result = movieStore.getMovies().stream()
+                .collect(Collectors.joining("!", "",""));
+        System.out.println(result);
+
           }
 }
--- kodilla-good-patterns/src/main/java/com/kodilla/good/patterns/MovieStore.java
+++ kodilla-good-patterns/src/main/java/com/kodilla/good/patterns/MovieStore.java
@@ -1,4 +1,4 @@
-package com.kodilla.stream.patterns;
+package com.kodilla.good.patterns;
 
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -7,7 +7,7 @@ import java.util.Map;
 
 public class MovieStore {
 
-    public static Map<String, List<String>> getMovies() {
+    public Map<String, List<String>> getMovies() {
         List<String> ironManTranslations = new ArrayList<>();
         ironManTranslations.add("Żelazny Człowiek");
         ironManTranslations.add("Iron Man");
