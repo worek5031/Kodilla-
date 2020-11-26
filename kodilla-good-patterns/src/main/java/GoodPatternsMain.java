015a21bb061c4555c85670c085c4c730568ee543
Piotr Worsztynowicz <piotr.worsztynowicz@iron-ic.com.pl>
2020-11-26 10:44:40 +0100

    2020-11-26.2

--- kodilla-good-patterns/src/main/java/GoodPatternsMain.java
+++ kodilla-good-patterns/src/main/java/GoodPatternsMain.java
@@ -6,8 +6,10 @@ public class GoodPatternsMain {
 
     public static void main(String[] args) {
         MovieStore movieStore = new MovieStore();
-        String result = movieStore.getMovies().stream()
+        String result = movieStore.getMovies().entrySet().stream()
+                .flatMap(s->s.getValue().stream())
                 .collect(Collectors.joining("!", "",""));
+
         System.out.println(result);
 
           }
