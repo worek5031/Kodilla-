2438deba5351eb27cd78b412c1c5e1c92f49bb07
Piotr Worsztynowicz <piotr.worsztynowicz@iron-ic.com.pl>
2020-12-03 22:30:34 +0100

    2020-12-03.5

--- kodilla-spring/src/main/java/com/kodilla/spring/calculator/Calculator.java
+++ kodilla-spring/src/main/java/com/kodilla/spring/calculator/Calculator.java
@@ -12,25 +12,21 @@ public final class Calculator {
     public double add(double a, double b) {
         double adding = a + b;
         display.displayValue(adding);
-        System.out.println(adding);
         return adding;
     }
     public double sub(double a, double b) {
         double substract = a - b;
         display.displayValue(substract);
-        System.out.println(substract);
         return substract;
         }
     public double mul(double a, double b) {
         double multiply = a * b;
         display.displayValue(multiply);
-        System.out.println(multiply);
         return multiply;
     }
     public double div(double a, double b) {
         double divided = a / b;
         display.displayValue(divided);
-        System.out.println(divided);
         return a / b;
     }
 
--- kodilla-spring/src/main/java/com/kodilla/spring/calculator/Display.java
+++ kodilla-spring/src/main/java/com/kodilla/spring/calculator/Display.java
@@ -1,17 +1,12 @@
 package com.kodilla.spring.calculator;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
 public final class Display {
 
-    @Autowired
-    Calculator calculator;
-
-
     public void displayValue(double val) {
-        System.out.println("Result is: ");
+        System.out.println("Result is: " + val);
             }
 
 }
